dfc4359d825530775d0953bffba9ca693a0a9ad3
Gabriel Choi <gabecreates@gmail.com>
2017-10-25 12:18:22 -0400

    markdown parser

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const markdownToJSON = require('gulp-markdown-to-json');
+const marked = require('marked');
+
+marked.setOptions({
+  pedantic: true,
+  smartypants: true
+});
+
+gulp.task('markdown', () => {
+  gulp.src('./posts/**/*.md')
+    .pipe(markdownToJSON(marked))
+    .pipe(gulp.dest('./api'))
+});
